776855225e089c0e38f5834c2eda6c839e5340d4
Jhonatan Rodriguez <jhonatanjrr@gmail.com>
2026-09-16 19:41:48 -0500

    Restaurar API keys para pruebas completas de la aplicacion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  tmdbApiKey: 'TU_TMDB_API_KEY',
-  tmdbReadAccessToken: 'TU_TMDB_READ_ACCESS_TOKEN',
+  tmdbApiKey: '6a9ea0030dab2539c4ba23670088811c',
+  tmdbReadAccessToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YTllYTAwMzBkYWIyNTM5YzRiYTIzNjcwMDg4ODExYyIsIm5iZiI6MTc4OTYwMTA1MS44MzIsInN1YiI6IjZhYWIyNTFiYzY1OTA3YTM3ZGM0OTMxNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bHD7agO52B-T7edJ1M-4dPUg85YM7AMClbd-dmL03ZQ',
   tmdbBaseUrl: 'https://api.themoviedb.org/3',
-  openWeatherApiKey: 'TU_OPENWEATHER_API_KEY',
+  openWeatherApiKey: '9f85bfbfd5f2b2c5ec73ed5692da510b',
   openWeatherBaseUrl: 'https://api.openweathermap.org/data/2.5'
 };
